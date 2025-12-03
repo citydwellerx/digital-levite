a3ab64bfbf1e0dbf81d30c212effb407a6216cc1
citydwellerx <163665034+citydwellerx@users.noreply.github.com>
2025-12-03 21:06:23 +0800

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -42,7 +42,7 @@ const App: React.FC = () => {
       {viewState === 'DETAIL' && !currentPost && (
         <div className="text-center py-20 border border-red-500 text-red-500">
           <h2 className="text-2xl font-bold mb-2">ERROR 404</h2>
-          <p>> DATA_CORRUPTED OR FILE_NOT_FOUND</p>
+          <p>&gt; DATA_CORRUPTED OR FILE_NOT_FOUND</p>
           <button 
             onClick={handleNavigateHome}
             className="mt-4 border border-red-500 px-4 py-2 hover:bg-red-500 hover:text-black"
