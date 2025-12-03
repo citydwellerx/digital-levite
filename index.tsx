fad73a3f08ec22e346093a65e1a650020216364c
citydwellerx <163665034+citydwellerx@users.noreply.github.com>
2025-12-03 22:18:54 +0800

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,15 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// This application has been converted to a single-file Vanilla JS architecture.
+// React logic in this file is disabled to prevent conflicts with index.html.
+console.log("The Digital Levite: Vanilla JS mode active.");
